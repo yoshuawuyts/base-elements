df663e56d7227b027f0ced30f66c8febcf48e257
Kristina Schneider <me@kristinaschneider.com>
2016-10-30 18:23:59 +0100

    fix(avatar): add height in order to avoid ugly ellipsis during waiting for image src to load

--- avatar.js
+++ avatar.js
@@ -7,7 +7,7 @@ module.exports = function (src, opts) {
   opts = opts || {}
 
   const cls = opts.class || ''
-  const size = 'w' + (opts.size || 3)
+  const size = 'w' + (opts.size || 3) + ' ' + 'h' + (opts.size || 3)
   const radius = 'br' + ((opts.radius)
     ? (opts.radius === 100)
       ? '-100'
@@ -19,7 +19,7 @@ module.exports = function (src, opts) {
   assert.equal(typeof cls, 'string', 'base-elements/avatar: cls should be a string')
   assert.equal(typeof size, 'string', 'base-elements/avatar: size should be a string')
   assert.equal(typeof radius, 'string', 'base-elements/avatar: radius should be a string')
-  assert.ok(size === 'w1' || size === 'w1' || size === 'w2' || size === 'w3' || size === 'w4' || size === 'w5', 'base-elements/avatar: size should be >= 1 && <= 5')
+  assert.ok(size === 'w1 h1' || size === 'w2 h2' || size === 'w3 h3' || size === 'w4 h4' || size === 'w5 h5', 'base-elements/avatar: size should be >= 1 && <= 5')
   assert.ok(radius === 'br-100' || radius === 'br0' || radius === 'br1' || radius === 'br2' || radius === 'br3' || radius === 'br4', 'base-elements/avatar: radius should be >= 0 && <= 4 OR 100')
 
   const clx = size + ' ' + radius + ' ' + cls
