462655a54b81891263086cd1f362fa0407b6ffd3
Tim Pinington <timpinington@gmail.com>
2017-03-13 10:24:02 +0000

    add file

---
+++ spacer.js
@@ -0,0 +1,20 @@
+var assert = require('assert')
+var html = require('bel')
+
+function flexStyle (count, direction) {
+  return `-webkit-box-flex: ${count}
+        -ms-flex: ${count}
+            flex: ${count}
+  `
+}
+
+module.exports = function (flex, direction) {
+  flex = flex || '1'
+  direction = direction || 'row'
+
+  assert.equal(typeof flex, 'string', 'base-elements/spacer: flex should be a string')
+  assert.equal(typeof direction, 'string', 'base-elements/spacer: direction should be a string')
+  assert.ok(direction === 'row' || direction === 'column', 'base-elements/spacer: direction must be row or column')
+
+  return html`<div class="flex-${direction}" style="${flexStyle(flex, direction)}"></div>`
+}
